0f90204731d4493a130c15ad0edf187d7a0f28dc
Harry Hu <hoozh1123@gmail.com>
2019-10-15 00:54:43 +1100

    First Commit

---
+++ src/build/precache-manifest.4289291becd32750ab8ee2c1f5a97918.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf8daebbd0eb5697a042eb477ee5688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cfa0cdb5d54e461d07e",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "4f448ca6965dbb275fe3",
+    "url": "/static/js/2.006dcf37.chunk.js"
+  },
+  {
+    "revision": "9cfa0cdb5d54e461d07e",
+    "url": "/static/js/main.4b9c6ddc.chunk.js"
+  },
+  {
+    "revision": "5d3df65c6aad5af6f7eb",
+    "url": "/static/js/runtime-main.8a83d180.js"
+  }
+]);
